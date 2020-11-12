4cd52b0ae274763031a6a01132a6c73bdccd4f1a
bugra4569 <bugraocalan@gmail.com>
2020-11-12 21:09:59 +0300

    Add project files.

---
+++ BlazorStateObject/ModelState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStateObject
+{
+    public class ModelState
+    {
+        public int Totalsum { get; set; } = 0;
+
+        public void AddNumber(int number)
+        {
+            Totalsum += number;
+        }
+        public void ResetSum()
+        {
+            Totalsum = 0;
+        }
+    }
+}
